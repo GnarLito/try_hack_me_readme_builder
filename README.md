cc090b9423098291dad1c65b34e8f1a692fe8d43
gnarlito <gamevaart21@gmail.com>
2021-09-17 22:45:39 +0200

    update with thm_api

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ Auto create README.md file for Try Hack Me rooms.
 Usage: THM_md_builder.py 
   -r <room name (from url)> 
   -o <output file path/name> 
-  [-c <connect-sid cookie> (only needed for auto fill of the README.md) ]
+  [-s <connect-sid cookie> (only needed for auto fill of the README.md) ]
+  [-a (dont auto fill in answers)]
 ```
 Note:
   The connect.sid cookie from THM is a static cookie which is not session based (as far as i know).
-  
 
+If u find any room that doesn't look good, feel free to make an issue for it.
 
 Feel free to edit/improve the script and create a pull request afterward!
--- THM_md_builder.py
+++ THM_md_builder.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
-import requests
-import json
-import sys, getopt
-from thmapi import THM
+import sys, getopt, os
+from thm_api.thmapi import THM
 from html_to_text import *
 
 
-def main(room, out_file=None, creds=None):
+def main(room, out_file=None, creds=None, skip_answers=False):
   thm = THM()
-
-  print(creds.__len__())
 
   if creds.__len__() > 0:
     thm.login(creds)
@@ -23,20 +19,20 @@ def main(room, out_file=None, creds=None):
     return 1;
   
   room_data = format_data(room_data)
-  Write_tasks(room, room_data, out_file)
+  Write_tasks(room, room_data, out_file, skip_answers)
 
 
-def Write_tasks(name, room_data, out_file):
-  out_string = "# "+ name;
+def Write_tasks(name, room_data, out_file, skip_answers):
+  out_string = "# "+ name + "\n\n";
   for task in room_data:
     # add TASK
-    out_string += f"# {task['taskTitle']}\n"
+    out_string += f"# {task['taskTitle'].strip()}\n"
     for quest in task['questions']:
       # add QUESTIONS
-      out_string += f"{quest['questionNo']}. **{quest['question']}**\n\n"
+      out_string += f"{quest['questionNo']}. **{quest['question'].strip()}**\n\n"
       out_string += " > "
       # if answer exist write it
-      if task['tasksInfo'].__len__() > 1 and task['tasksInfo'][int(quest['questionNo'])-1]['correct']:
+      if not skip_answers and task['tasksInfo'].__len__() > 1 and task['tasksInfo'][int(quest['questionNo'])-1]['correct']:
       
         if not task['tasksInfo'][int(quest['questionNo'])-1]['noAnswer']:
           out_string += f"{task['tasksInfo'][int(quest['questionNo'])-1]['submission'].strip()}\n"
@@ -48,36 +44,52 @@ def Write_tasks(name, room_data, out_file):
 
       out_string += "\n"
 
+  if out_file is None:
+    out_file = f"./{name}/README.md"
+  if out_file.endswith("/") or out_file.endswith("\\"):
+    out_file += "README.md"
+  elif not out_file.endswith(".md"):
+    out_file += ".md"
+
+  os.makedirs(os.path.dirname(out_file), exist_ok=True)
   with open(f"{out_file}", "w") as out:
     out.write(out_string)
-  print(f"file write done | in: {out_file}")
+  print(f"Output written to {out_file}")
 
 
 
 if __name__ == "__main__":
   room = None
   out_file = None
-  cred = {};
+  cred = {}
+  skip_answers = False
   try:
-    opts, args = getopt.getopt(sys.argv[1:],"hr:o:u:p:s:",["help","ofile=","room=", "username=", "password=", "session="])
+    opts, args = getopt.getopt(sys.argv[1:],"hr:o:u:p:s:a",["help","ofile=","room=", "username=", "password=", "session=", "no_answers"])
     for opt, arg in opts:
       if opt in ("-h", "--help"):
-        print(f"{sys.argv[0]} -r <room> -o <outFile> [-c <cookie: connect.sid>]")
+        print(f"{sys.argv[0]} -r <room> -o <outFile> [-s <session: connect.sid> [-a (dont fill answers)]]")
         exit(0)
       elif opt in ("-r", "--room"):
         room = arg
       elif opt in ("-o", "--ofile"):
         out_file = arg
       elif opt in ("-u","--username"):
+        print("User accout doesn't yet work :(\nUse session instead")
+        exit(1)
         cred.update({"username": arg})
       elif opt in ("-p","--passowrd"):
-            cred.update({"password": arg})
+        print("User accout doesn't yet work :(\nUse session instead")
+        exit(1)
+        cred.update({"password": arg})
       elif opt in ("-s","--session"):
         cred.update({"session": arg})
       
+      elif opt in ("-a","--no_answer"):
+        skip_answers = True
+      
   
   except getopt.GetoptError:
-    print(f"{sys.argv[0]} -r <room> -o <outFile> [-c <cookie: connect.sid>]")
+    print(f"{sys.argv[0]} -r <room> -o <outFile> [-s <session: connect.sid> [-a (dont fill answers)]]")
     sys.exit(2)
 
   if room is None:
@@ -97,4 +109,4 @@ if __name__ == "__main__":
     print("missing the Session.\nsee '-h'")
     exit(1)
   
-  main(room, out_file, cred)
+  main(room, out_file, cred, skip_answers)
