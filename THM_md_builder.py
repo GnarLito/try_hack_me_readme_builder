161e4dce4712513b38573cda35450d250e4f1460
GnarLtito <62666800+GnarLito@users.noreply.github.com>
2021-12-12 23:15:34 +0100

    Tryhackme.py package rewrite (#5)
    updated to use tryhackme.py package

--- THM_md_builder.py
+++ THM_md_builder.py
@@ -1,29 +1,98 @@
 #!/usr/bin/env python3
 
+import os
 import getopt
 import sys
+import re
+import tryhackme
+import config
 
-from room_tasks import room_task
-from thm_api.thmapi import THM
+def get_attr(thm: tryhackme.Client, room: tryhackme.Room, task: tryhackme.RoomTask=None, question: tryhackme.Question=None):
+    attr = {
+      "room_name":       room.name
+      , "authenticated": thm.authenticated
+      , "task_count":    room.tasks.__len__()
+    }
+    for slot in list(filter(lambda a : not a.startswith("_"),list(tryhackme.Room.__slots__))):
+      attr["room_" + slot] = getattr(room, slot)
+    
+    if task is not None:
+      task_slots = ("question_count", )
+      for slot in list(filter(lambda a : not a.startswith("_"),list(tryhackme.RoomTask.__slots__))):
+        attr["task_" + slot] = getattr(task, slot)
+      for task_slot in task_slots:
+        attr["task_" + slot] = getattr(task, task_slot)
+      
+    if question is not None:
+      for slot in list(filter(lambda a : not a.startswith("_"),list(tryhackme.Question.__slots__))):
+        attr["question_" + slot] = getattr(question, slot)
+    
+    return attr
 
-
-def main(room_name, out_file=None, creds=None, skip_answers=False):
-  thm_session = THM()
-
-  if creds.__len__() > 0:
-    thm_session.login(creds)
-    pass
   
-  room = room_task(thm_session, room_name, skip_answers)
-  room.write_room(out_file)
+def replace_attr(rstring: str, pattrn: str, attrs:dict):
+  for tag in re.findall(pattrn, rstring):
+    if tag not in attrs: continue
+    rstring = rstring.replace("{"+tag+"}", str(attrs[tag]))
+  return rstring
+
+def write_room(file_loc: str, write_string):
+    if file_loc is None:                                  file_loc = f"./README.md"
+    if file_loc.endswith("/") or file_loc.endswith("\\"): file_loc += "README.md"
+    elif not file_loc.endswith(".md"):                    file_loc += ".md"
+      
+    if '/' in file_loc or '\\' in file_loc:
+      try: os.makedirs(os.path.dirname(file_loc), exist_ok=True)
+      except: 
+        print("couldn't create directory..")
+        return
+
+    with open(f"{file_loc}", "w") as out:
+      out.write(write_string)
+    print(f"Output written to {file_loc}")
+
+def main(room_name, out_file=None, session=None, skip_answers=False):
+  thm = tryhackme.Client(session)
+
+  room = thm.get_room(room_name)
+
+  tasks_string = ""
+  for task in room.tasks:
+    
+    task_attrs = get_attr(thm, room, task)
+    questions_string = ""
+    for question in task.questions:
+      question_attr = get_attr(thm, room, question=question)
+      question_attr.update(task_attrs)
+      if not question.has_answer and question.correct:
+        question.submission = replace_attr(config.template['no_answer'], r"{(.*?)}", question_attr)
+      elif not question.correct:
+        question.submission = replace_attr(config.template['placeholder'], r"{(.*?)}", question_attr)
+    
+      if skip_answers:
+        questions_string += replace_attr(config.template["question"].replace("{question_submission}", ""), r"{(.*?)}", question_attr)
+      else:
+        questions_string += replace_attr(config.template["question"], r"{(.*?)}", question_attr)
+    
+    task_attrs.update({"questions": questions_string})
+    tasks_string += replace_attr(config.template["task"], r"{(.*?)}", task_attrs)
+
+  room_attrs = get_attr(thm, room)
+  room_attrs.update({"tasks": tasks_string})
+  out_string = replace_attr(config.template["room"], r"{(.*?)}", room_attrs)
+  write_room(out_file, out_string)
+  
+
+
+
 
 if __name__ == "__main__":
   room = None
   out_file = None
-  cred = {}
+  session = None
   skip_answers = False
   try:
-    opts, args = getopt.getopt(sys.argv[1:],"hr:o:u:p:s:a",["help","ofile=","room=", "username=", "password=", "session=", "no_answers"])
+    opts, args = getopt.getopt(sys.argv[1:],"hr:o:s:a",["help","ofile=","room=", "session=", "no_answers"])
     for opt, arg in opts:
       if opt in ("-h", "--help"):
         print(f"{sys.argv[0]} -r <room> -o <outFile> [-s <session: connect.sid> [-a (dont fill answers)]]")
@@ -32,16 +101,8 @@ if __name__ == "__main__":
         room = arg
       elif opt in ("-o", "--ofile"):
         out_file = arg
-      elif opt in ("-u","--username"):
-        print("User accout doesn't yet work :(\nUse session instead")
-        exit(1)
-        cred.update({"username": arg})
-      elif opt in ("-p","--passowrd"):
-        print("User accout doesn't yet work :(\nUse session instead")
-        exit(1)
-        cred.update({"password": arg})
       elif opt in ("-s","--session"):
-        cred.update({"session": arg})
+        session = arg
       
       elif opt in ("-a","--no_answer"):
         skip_answers = True
@@ -55,14 +116,4 @@ if __name__ == "__main__":
     print("Room needs be supplied\nsee '-h'")
     exit(1)
   
-  if   cred.__len__() > 0 and "session"  not in cred and "username" not in cred:
-    print("Missing the Username.\nsee '-h'")
-    exit(1)
-  elif cred.__len__() > 0 and "session"  not in cred and "password" not in cred:
-    print("Missing the Password.\nsee '-h'")
-    exit(1)
-  elif cred.__len__() > 0 and "username" not in cred and "session"  not in cred:
-    print("missing the Session.\nsee '-h'")
-    exit(1)
-  
-  main(room, out_file, cred, skip_answers)
+  main(room, out_file, session, skip_answers)
--- config.md
+++ config.md
@@ -12,46 +12,62 @@ More room options will be on the way.
 
 ### ROOM:
 ```ini
-room_name       = the name of the room
-authenticated   = true if signed in (session)
-tasks_count     = the number of tasks in room
-tasks           = sets where tasks get printed
+room_name           = the name of the room
+authenticated       = true if signed in (session)
+room_id             = Room id
+room_title          = title is the same as the name
+room_description    = description of the room
+room_created        = time of creation
+room_published      = time of publishing
+room_users          = number of users that have joined the room
+room_type           = room type
+room_public         = if the room is public 
+room_difficulty     = room difficulty
+room_freeToUse      = if the room is free to use
+room_ctf            = if the room is a ctf
+room_tags           = the room tags
+room_ipType         = the ip type of the room
+room_simpleRoom     = if the room is considerd simple
+room_locked         = if the room is locked
+room_comingSoon     = if the room is coming out soon
+room_views          = number of views
+room_certificate    = if the room has a certificate to achieve
+room_timeToComplete = time in seconds to complete
+room_userCompleted  = if the room is already completed
+tasks_count         = the number of tasks in room
+tasks               = sets where tasks get printed
 ```
 ### TASKS:
 ```ini
-taskTitle       = Name of the task
-taskType        = type of the task (VM, downloadable, none)
-taskNo          = task number
-taskCreated     = creation time of the task (Ex. "2019-08-01T17:02:09.503Z")
-uploadId        = Id for downloads/starting VM
-questions_count = the number of questions in the task
-questions       = sets where questions get printed
+task_title              = name of the task
+task_description        = description of the task
+task_deadline           = deadline of the task
+task_type               = type of the task (VM, downloadable, none)
+task_number             = task number
+task_created            = creation time of the task (Ex. "2019-08-01T17:02:09.503Z")
+task_uploadId           = Id for downloads/starting VM
+task_questions_count    = the number of questions in the task
+questions               = sets where questions get printed
 ```
 ### QUESTIONS:
 ```ini
-question        = Question name
-questionNo      = Question number
+question_question = question name
+question_number   = question number
+question_hint     = question hint
 ```
 >When signed in (session) can u access the following:
 ```ini
-hint            = question hint
-correct         = only true when correct answer given
-extraPoints     = amount of extra point given for this question
-attempts        = attempts before the answer was correct
-submission      = the answer given
-noAnswer        = true if no answer needed for the question
-answerDesc      = placeholder text in answer textarea
+question_description    = placeholder text in answer textarea
+question_correct        = if a correct answer has been given
+question_extra_points   = amount of extra point given for this question
+question_attempts       = attempts before the answer was correct
+question_submission     = the answer given
+question_has_answer     = if the question needs an answer
 ```
 ### miscellaneous
 - `no_answer` and `placeholder` can use all template options above, to use them encapsulate the option between `{}`
 ```ini
-no_answer       = sets the submission field when `noAnswers` is set
+no_answer       = sets the submission field when `has_answers` is not set
 placeholder     = sets the submission field when `correct` is not set
 ```
 ___
-## CONFIG OPTIONS:
-```ini
-REPLACE_TAGS    = list set for replacing HTML codes
-HTML_TAGS       = list set for removing HTML tags
-auth_only       = set of options inaccessable when not signed in (session)
-```
